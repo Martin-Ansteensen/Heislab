5dfd6123930a7175ec24c36be989ce7964ef6857
Martin Ansteensen <martin.ansteensen@outlook.com>
2025-02-18 14:03:06 +0100

    Create que as linked list. Update Makefile to allow compiling of que test

---
+++ project/source/que.c
@@ -0,0 +1,137 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <signal.h>
+
+#include "que.h"
+
+Node* create_node(int floor, MotorDirection dir)
+{
+    // Allocate the new node_p
+    Node* node_p = (Node*)malloc(sizeof(Node));
+    if (node_p == NULL)
+    {
+        perror("Memory allocation failed!\n");
+    }
+    // Init the new node_p
+    node_p->parent_p = NULL;
+    node_p->child_p = NULL;
+    node_p->floor = floor;
+    node_p->dir = dir;
+
+    return node_p;
+}
+
+Node* que_get_tail_node_p(Node** head_pp)
+{
+    // The que is empty if head_pp points to NULL
+    if (*head_pp == NULL) return *head_pp;
+
+    // Find the tail Node by locating the node_p without a child
+    Node* tail_p = *head_pp;
+    while (1)
+    {
+        if(tail_p->child_p == NULL)
+        {
+            return tail_p;
+        }
+        else
+        {
+            tail_p = tail_p->child_p;
+        }
+    }
+}
+
+
+Node* que_add_node_back(Node** head_pp, int floor, MotorDirection dir)
+{
+    Node* new_node_p = create_node(floor, dir);
+    
+    if (*head_pp == NULL)
+    {
+        // The que is empty, make head_pp point to the new node_p
+        *head_pp = new_node_p;
+    }
+    else 
+    {
+        Node* tail_p = que_get_tail_node_p(head_pp);
+    
+        // The new node_p is the tail
+        tail_p->child_p = new_node_p;
+        new_node_p->parent_p = tail_p;
+    }
+    return new_node_p;
+}
+
+void que_delete_node(Node** head_pp, Node* node_p)
+{
+    // The que is empty, nothing to delete
+    if (node_p == NULL) return;
+
+    if (node_p->parent_p == NULL)
+    {
+        // We want to delete the head_pp in a non empty que
+        if (node_p->child_p != NULL)
+        {
+            // Assign the new head_p
+            *head_pp = node_p->child_p;
+            (*head_pp)->parent_p = NULL;
+        }
+        else
+        {
+            // There is no new head_pp to appoint
+            *head_pp = NULL;
+        }
+    
+    }
+    else
+    {
+        // Update linked list by linking the parent and child of the node
+        // we are deleting  
+        node_p->parent_p->child_p = node_p->child_p;
+        // The node we are deleting might be the tail, meaning it does not
+        // have a child.
+        if (node_p->child_p != NULL){
+            node_p->child_p->parent_p = node_p->parent_p;
+
+        }
+    }
+    
+    free(node_p);
+}
+
+void que_delete(Node** head_pp)
+{
+    Node* tail_p = que_get_tail_node_p(head_pp);
+
+    // The que is already empty, no need to delete any nodes
+    if (tail_p == NULL) return;
+
+    while(tail_p->parent_p != NULL)
+    {
+        // Save the parent of the tail, which will be the new tail
+        Node* new_tail_p = tail_p->parent_p;
+        que_delete_node(head_pp, tail_p);
+        // Assign the tail to be the parent of the old tail
+        tail_p = new_tail_p;
+    }
+
+    // Free head node
+    que_delete_node(head_pp, *head_pp);
+}
+
+void print_que_forward(Node** head_pp) {
+    Node* temp_p = *head_pp;
+    // There is nothing to print in an empty que
+    if (*head_pp == NULL) printf("Empty");
+
+    int first_node = 1;
+
+    while (temp_p != NULL)
+    {
+        printf(first_node ? "" : "<->");
+        first_node = 0;
+        printf("(floor: %i, dir: %i)", temp_p->floor, temp_p->dir);
+        temp_p = temp_p->child_p;
+    }
+    printf("\n");
+}
---
+++ project/source/que.h
@@ -0,0 +1,17 @@
+#pragma once
+#include "driver/elevio.h"
+
+typedef struct Node {
+    struct Node* parent_p;
+    struct Node* child_p;
+    int floor;
+    MotorDirection dir;
+} Node;
+
+
+Node* create_node(int floor, MotorDirection dir);
+Node* que_get_tail_node_p(Node** head_pp);
+Node* que_add_node_back(Node** head_pp, int floor, MotorDirection dir);
+void que_delete_node(Node** head_pp, Node* node_p);
+void que_delete(Node** head_pp);
+void print_que_forward(Node** head_pp);
